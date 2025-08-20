0d5e086584b724b06d8ec532a3baaef81ffd7600
bijith <informbijith@gmail.com>
2025-08-20 16:03:04 +0530

    add register/profile/list/update serializers

---
+++ users/serializers.py
@@ -0,0 +1,97 @@
+from typing import Any
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import Role
+
+
+User = get_user_model()
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = ["id", "name"]
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    role = serializers.SlugRelatedField(slug_field="name", queryset=Role.objects.all(), required=False, allow_null=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "password", "role"]
+
+    def validate_password(self, value: str) -> str:
+        validate_password(value)
+        return value
+
+    def create(self, validated_data: dict[str, Any]) -> User:
+        password = validated_data.pop("password")
+        request = self.context.get('request')
+        desired_role = validated_data.pop('role', None)
+
+        # Default role to EMPLOYEE unless an authenticated admin is creating the user
+        role_to_set = None
+        if request and request.user and request.user.is_authenticated and getattr(request.user, 'is_admin', False):
+            role_to_set = desired_role
+        if role_to_set is None:
+            role_to_set, _ = Role.objects.get_or_create(name=Role.EMPLOYEE)
+
+        user = User(**validated_data)
+        user.role = role_to_set
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    role = RoleSerializer()
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "role"]
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    role = RoleSerializer()
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name", "role", "date_joined"]
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    role = serializers.SlugRelatedField(slug_field="name", queryset=Role.objects.all(), required=False, allow_null=True)
+    password = serializers.CharField(write_only=True, required=False, allow_null=True, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "first_name", "last_name", "role", "password"]
+
+    def update(self, instance: User, validated_data: dict[str, Any]) -> User:
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['role'] = user.role.name if getattr(user, 'role', None) else None
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['user'] = {
+            'id': self.user.id,
+            'username': self.user.username,
+            'email': self.user.email,
+            'role': self.user.role.name if getattr(self.user, 'role', None) else None,
+        }
+        return data
